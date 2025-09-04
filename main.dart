48f6f724fb15f05216b6da5bc0e47c468bdc6ab6
CourageMafuma <courageychikwanday@gmail.com>
2025-09-04 17:12:27 +0200

    first commit

---
+++ main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:calendar_manager/main.dart'; // points to your actual app entry in lib/
+
+void main() {
+  runApp(
+      const MyApp()); // or whatever your main widget is called in lib/main.dart
+}
